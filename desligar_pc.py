ffc16a58b61372e043a75fdb86d06f2d064b51d2
Gilson-barbosa-dev <97996435+Gilson-barbosa-dev@users.noreply.github.com>
2022-07-01 21:50:59 -0300

    V2 - com Radio
    Melhorado o título e botões

--- desligar_pc.py
+++ desligar_pc.py
@@ -1,12 +1,16 @@
 from logging import shutdown
 import os
 from pickle import FALSE, TRUE
+from tkinter.tix import Tree
 import PySimpleGUI as sg
+
+desligar = 'shutdown /s /t'
+cancelar = 'shutdown -a'
 
 #Layout do desligamento
 layout = [ 
         [sg.Text('Tempo de desligamento'), sg.Input(key='tempo', size=(15,6))],
-        [sg.Text('Escolha a ação            '),sg.Combo(['shutdown /s /t','shutdown -a',],key='acao', size=(13,6))],
+        [sg.Radio('Desligar',desligar, key='acao'), sg.Radio('Cancelar',cancelar)],
         [sg.Button('Avançar')]
 ]
 
@@ -17,15 +21,18 @@ Janela = sg.Window('Desligar PC', layout)
 #Leitura dos dados
 
 while TRUE:
-    eventos, valores = Janela.read()
-    if eventos == sg.WINDOW_CLOSED:
+        eventos, valores = Janela.read()
+        if eventos == sg.WINDOW_CLOSED:
             break
     
-    tempo = valores['tempo']
-    acao = valores['acao']
+        tempo = valores['tempo']
+        acao = valores['acao']
 
-print(acao)
-
+        if acao == True:
+                acao = desligar
+        elif acao == False:
+                acao = cancelar        
+        
 Janela.close()
 
 os.system(f'{acao} {tempo}')
